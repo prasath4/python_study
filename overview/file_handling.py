278f3a8f9a9ffcb4e780af1c4519aff898688078
prasath4 <prasath3428@gmail.com>
2025-02-28 20:53:43 +0530

    add file_handling

---
+++ overview/file_handling.py
@@ -0,0 +1,45 @@
+# file_Path= r"C:\Users\lenovo\Desktop\workspace\Python\File.txt"
+
+# f= open (file_Path,'r')
+# txt= f.read()
+# print(type(txt))
+# print(txt)
+# f.close()
+
+# read= f.readlines()
+# read=f.readline()
+
+
+# Changing JSON to Dictionary
+import json
+# JSON
+person_json = '''{
+    "name": "Asabeneh",
+    "country": "Finland",
+    "city": "Helsinki",
+    "skills": ["JavaScrip", "React", "Python"]
+}'''
+# let's change JSON to dictionary
+person_dct = json.loads(person_json)
+print(type(person_dct))
+print(person_dct)
+print(person_dct['name'])
+
+
+
+
+# Changing Dictionary to JSON
+import json
+# python dictionary
+person = {
+    "name": "Asabeneh",
+    "country": "Finland",
+    "city": "Helsinki",
+    "skills": ["JavaScrip", "React", "Python"]
+}
+# let's convert it to  json
+person_json = json.dumps(person, indent=4) # indent could be 2, 4, 8. It beautifies the json
+print(type(person_json))
+print(person_json)
+
+
